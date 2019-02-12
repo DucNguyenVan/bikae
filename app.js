9ac0359b5e7e8328f7f3f66503aada57c8518eea
DucNguyenVan <duc.nv227@gmail.com>
2019-02-12 21:30:10 +0900

    [ADD] adblock

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const puppeteer = require('puppeteer');
 (async () => {
 const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
   const page = await browser.newPage();
+  await page.setRequestInterception(true);
+  page.on('request', interceptedRequest => {
+    if (!interceptedRequest.url().includes('bikae.net'))
+      interceptedRequest.abort();
+    else
+      interceptedRequest.continue();
+  });
+  console.log("aaaaaaa")
   await page.goto('https://bikae.net/ngu-phap/tong-hop-ngu-phap-n2/');
   //rd_idx = await page.$$eval('.entry-content li', a => {sum = a.length; idx = (Math.random(0, sum/100)*100).toFixed(); return a});
   const href = await page.evaluate(() => {
