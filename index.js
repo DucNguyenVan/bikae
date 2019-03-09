df780703bdd88a3050b2183113eb0f791002a70a
DucNguyenVan <duc.nv227@gmail.com>
2019-03-09 11:36:19 +0900

    [ADD] auto response

--- index.js
+++ index.js
@@ -82,7 +82,13 @@ function handleMessage(sender_psid, received_message) {
   let response;
 
   // Check if the message contains text
-  if (received_message.text && received_message.text.toLowerCase() == "next") {
+  if (!received_message.text) {
+    return;
+  }
+  if (received_message.text.toLowerCase() != "next") {
+    callSendAPI(sender_psid, {"text": "Type [next] to get a new grammar."});
+  }
+  else if (received_message.text && received_message.text.toLowerCase() == "next") {
     (async () => {
       const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
         const page = await browser.newPage();
@@ -94,22 +100,21 @@ function handleMessage(sender_psid, received_message) {
             interceptedRequest.continue();
         });
         await page.goto('https://bikae.net/ngu-phap/tong-hop-ngu-phap-n2/');
-        const href = await page.evaluate(() => {
-          var sum = document.querySelectorAll('.entry-content li a').length
+        const grammar_href = await page.evaluate(() => {
+          var grammar_selectors = '.entry-content span a';
+          var sum = document.querySelectorAll(grammar_selectors).length
           const rd_idx = (Math.random(0,sum/100)*100).toFixed();
-          return document.querySelectorAll('.entry-content li span a')[rd_idx].href
+          return document.querySelectorAll(grammar_selectors)[rd_idx].href;
         })
-        console.log(href)
-        await page.goto(href);
+        await page.goto(grammar_href);
         const grammar = await page.evaluate(() => {
           return document.querySelector('.entry-header .entry-title').textContent
         })
-        console.log(grammar)
         await browser.close();
 
       // Create the payload for a basic text message
         response = {
-          "text": `${grammar} \n ${href}`
+          "text": `${grammar} \n ${grammar_href}`
         }
          // Sends the response message
         callSendAPI(sender_psid, response);
